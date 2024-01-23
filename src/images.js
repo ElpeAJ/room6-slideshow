16fffcd38ef1974cf02b8a290351b0a17f144ff5
Elpedia Junior Arthur <elpediaarthurjunior@gmail.com>
2024-01-23 15:00:22 -0800

    Improving on the logic of the code

--- src/images.js
+++ src/images.js
@@ -1,28 +1,47 @@
+//code for displaying images
 const images = [
-  "./images/sunflower-lady.jpeg",
-  "./images/autumn-lady.jpeg",
-  "./images/swimming-lady.jpeg",
-  "./images/happy-lady.jpeg",
-  "./images/lady-dancing.jpeg",
-  "./images/lady-enjoying.jpeg",
-  "./images/hiking-lady.jpeg",
-  "https://source.unsplash.com/",
+  "./images/sunflower-lady.jpeg", //0
+  "./images/autumn-lady.jpeg", //1
+  "./images/swimming-lady.jpeg", //2
+  "./images/happy-lady.jpeg", //3
+  "./images/lady-dancing.jpeg", //4
+  "./images/lady-enjoying.jpeg", //5
+  "./images/hiking-lady.jpeg", //6
+  "https://source.unsplash.com/", //7
 ];
 
-let currentImageIndex = 0;
-let totalImage = images.length;
+let currentImageIndex = 0; //0
+let totalImage = images.length; // 8
 
+// looping to go forward and backwards
 function goBack() {
   if (currentImageIndex === 0) {
-    currentImageIndex = totalImage - 1;
+    // is 0 equal to 0?
+    currentImageIndex = totalImage - 1; //8 - 1
   } else {
-    currentImageIndex -= 1;
+    currentImageIndex -= 1; //1 0  // 2 1
   }
+  main.style.backgroundImage = `url(${images[currentImageIndex]})`;
 }
+
 function goForward() {
   if (currentImageIndex === totalImage - 1) {
+    // is 7 equal to 7?
     currentImageIndex = 0;
   } else {
     currentImageIndex += 1;
   }
+  main.style.backgroundImage = `url(${images[currentImageIndex]})`;
 }
+
+// function goBack() {
+//   if (currentImageIndex !== 0) {
+//     currentImageIndex -= 1; //1 0  // 2 1
+//   }
+// }
+
+// function goForward() {
+//   if (currentImageIndex !== totalImage - 1) {
+//     currentImageIndex += 1;
+//   }
+// }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
+// code the gets the slide show going
 const main = document.querySelector("main");
 main.style.backgroundImage = `url(${images[0]})`;
 
 function goto() {
-  main.style.transition = `red 1.5s linear 0.5s`;
-  main.style.backgroundImage = `url(${images[currentImageIndex]})`;
+  // main.style.transition = `red 1.5s linear 0.5s`;
+  // main.style.backgroundImage = `url(${images[currentImageIndex]})`;
 }
 
 leftButton.addEventListener("click", function () {
   goBack();
-  goto();
+  // goto();
 });
 
 rightButton.addEventListener("click", function () {
   goForward();
-  goto();
+  // goto();
 });
 
-// setInterval(function () {
-//   goForward();
-//   goto();
-// }, 1300);
+setInterval(function () {
+  goForward();
+  goto();
+}, 1300);
